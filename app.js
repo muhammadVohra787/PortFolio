c8432544ba83e5eb532fcd86306fa92a45f7a4ef
Muhammad Vohra <mvohra10@my.centennialcollege.ca>
2023-09-27 02:45:18 -0400

    third commit

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.use('/img',express.static(__dirname + 'public/img'))
+app.use('/js',express.static(__dirname + 'public/js'))
 app.use('/css',express.static(__dirname + 'public/css'))
+app.set('views', './app/views');
+app.set('view engine', 'ejs');
 
 app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/home.html');
+  res.render('home.ejs')
 });
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/home.html');
+  res.render('home.ejs')
 });
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/about.html');
+  res.render('about.ejs')
 });
-
+app.get('/projects', (req, res) => {
+  res.render('projects.ejs')
+});
+app.get('/services', (req, res) => {
+  res.render('service.ejs')
+});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
---
+++ public/js/services.js
@@ -0,0 +1,21 @@
+var btn1 = document.getElementById('service1');
+var btn2= document.getElementsByClassName('service2');
+var btn3= document.getElementsByClassName('service3');
+var btn4= document.getElementsByClassName('service4');
+
+btn1.onclick = function() {
+    btn1.style="color:red;"
+    console.log('okay');
+};
+
+btn2.onclick = function() {
+    console.log('okay');
+};
+
+btn3.onclick = function() {
+    console.log('okay');
+};
+
+btn4.onclick = function() {
+    
+};
